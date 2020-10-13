594d9ece76b6450ad1cc2505df986c2918d5d63e
sexin-github <72774255+sexin-github@users.noreply.github.com>
2020-10-13 10:14:16 +0800

    Update index_component.py

--- databases/ths/index_component.py
+++ databases/ths/index_component.py
@@ -9,10 +9,10 @@ import json
 #root:root@localhost:3306/test"
 def trading_date():
     thsLogin = THS_iFinDLogin('htqh1015','990252')
-#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:Quant2020!@192.168.1.3:3306/ashare_new1"))
-#    db = dl.Db('192.168.1.3','root','Quant2020!','ashare_new1',3306)
-    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:root@localhost:3306/ths"))
-    db = dl.Db('10.3.135.14','root','root','ths',3306)
+#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@192.168.1.3:3306/ashare_new1"))
+#    db = dl.Db('192.168.1.3','root','x','ashare_new1',3306)
+    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@localhost:3306/ths"))
+    db = dl.Db('10.3.135.14','root','x','ths',3306)
     if not (thsLogin == 0 or thsLogin == -201):
         print("登录失败")
     else:
@@ -26,10 +26,10 @@ def trading_date():
 
 def index_components(date_,symbol_):
     thsLogin = THS_iFinDLogin('htqh1015','990252')
-#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:Quant2020!@192.168.1.3:3306/ashare_new1"))
-#    db = dl.Db('192.168.1.3','root','Quant2020!','ashare_new1',3306)
-    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:root@localhost:3306/ths"))
-    db = dl.Db('10.3.135.14','root','root','ths',3306)
+#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@192.168.1.3:3306/ashare_new1"))
+#    db = dl.Db('192.168.1.3','root','x','ashare_new1',3306)
+    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@localhost:3306/ths"))
+    db = dl.Db('10.3.135.14','root','x','ths',3306)
 
     if not (thsLogin == 0 or thsLogin == -201):
     # if False:
@@ -50,8 +50,8 @@ def index_components(date_,symbol_):
 
 def get_last_trading_date(start,end):
     
-#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:Quant2020!@192.168.1.3:3306/ashare_new1"))
-    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:root@localhost:3306/ths"))
+#    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@192.168.1.3:3306/ashare_new1"))
+    conn_LOCAL_mysql = sqlalchemy.create_engine(str(r"mysql+pymysql://root:x@localhost:3306/ths"))
     df = pd.read_sql(sql=f'select trade_date from sys_trade_date where exchange = "SZSE"',con=conn_LOCAL_mysql)
 #    df = df[df.trade_date < datetime.date(2001,1,1)]
 #    df = df[df.trade_date >= datetime.date(2000,1,1)]
